774e8d3d2764bc96a94bbabdc59b0cf4c51f59f2
Paul <onion8818@gmail.com>
2025-07-09 12:30:39 -0500

    part 2

--- tic_tac_toe_board.py
+++ tic_tac_toe_board.py
@@ -9,3 +9,19 @@ class TicTacToeBoard:
     
     def is_my_turn(self, i_am: str) -> bool:
         return self.player_turn == i_am
+
+    def make_move(self, index: int) -> None:
+        if self.state != "is_playing":
+            print("Game is already over.")
+            return
+        if not (0 <= index < 9):
+            print("Invalid index. Choose 0-8.")
+            return
+        if self.positions[index]:
+            print("Spot taken, try again.")
+            return
+
+        self.positions[index] = self.player_turn
+        print(self) 
+
+
