5c5ae8ee7de156cc2e6bb753e6a718e04cb9077a
Paul <onion8818@gmail.com>
2025-07-10 10:42:38 -0500

    update

--- tic_tac_toe_board.py
+++ tic_tac_toe_board.py
@@ -1,11 +1,10 @@
 from dataclasses import dataclass, field
-from typing import List
 
 @dataclass
 class TicTacToeBoard:
     state: str = "is_playing"
     player_turn: str = "x"
-    positions: List[str] = field(default_factory=lambda: [""] * 9)
+    positions: list[str] = field(default_factory=lambda: [""] * 9)
     
     def is_my_turn(self, i_am: str) -> bool:
         return self.player_turn == i_am
